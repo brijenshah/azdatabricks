9095fc0746d09f2ab40f37ca640794a7e52d7b41
Brijen Shah <brijen.shah@live.com>
2018-10-16 11:10:37 -0700

    Added Steps

--- notebooks/github-link.py
+++ notebooks/github-link.py
@@ -1,4 +1,22 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ##WordCount Example
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Step 1: Load text file from our Hosted Datasets. Shift-Enter Runs the code below.
+
+# COMMAND ----------
+
 filePath = "dbfs:/databricks-datasets/SPARK_README.md" # path in Databricks File System
 lines = sc.textFile(filePath) # read the file into the cluster
 lines.take(10) # display first 10 lines in the file
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC #### Step 2: Inspect the number of partitions (workers) used to store the dataset
+
+# COMMAND ----------
+
