671079409433adec976b27d298365ecd91bc9c04
Brijen Shah <brijen.shah@live.com>
2018-10-16 11:00:05 -0700

    Draft

---
+++ notebooks/github-link.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+filePath = "dbfs:/databricks-datasets/SPARK_README.md" # path in Databricks File System
+lines = sc.textFile(filePath) # read the file into the cluster
+lines.take(10) # display first 10 lines in the file
